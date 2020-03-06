f71db79908635cff2787969ee384be2a55b03a20
hima atluri <himain8977@gmail.com>
2020-03-06 01:18:30 -0600

    Custom Module to deploy a sample VM

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,8 +1,27 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-
 module "aws_ec2" {
-  source = "../modules/ec2"
+  source = "../../modules/ec2"
+
+  // inputs to module
+  instance_type = "t2.micro"
+
+  // ssh-key-material
+  pub_keypath = "~/.ssh/id_rsa.pub"
+
+  // network configs
+  vpc_id = "vpc-b853c8c2"
+  subnet_id = "subnet-09abb443"
+
+  //ports to expose
+  security_group_ports = "${map(
+    "http", "80",
+    "https", "443"
+    ,"ssh", "22"
+  )}"
+
+  //tags
+  default_tags = "${map(
+    "Name", "terri-project",
+    "app", "terri-project-web-service",
+    "env", "dev"
+  )}"
 }
